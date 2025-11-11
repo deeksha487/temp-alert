d5efc7b6fac48c87350d4d4400d9365b571d0c0f
deeksha487 <deekshavernekar11@gmail.com>
2025-11-11 11:01:13 +0530

    Update temp1.py

--- temp1.py
+++ temp1.py
@@ -1,27 +1,14 @@
 import sys
-import time
 
-def temperature_alert(temp):
+# check if argument is given
+if len(sys.argv) < 2:
+    print("Please give temperature as argument.")
+else:
+    temp = float(sys.argv[1])
+
     if temp < 15:
-        print(f"Temperature: {temp}°C → Cold ❄")
-    elif 15 <= temp <= 30:
-        print(f"Temperature: {temp}°C → Normal 🌤")
+        print("Cold")
+    elif temp <= 30:
+        print("Normal")
     else:
-        print(f"Temperature: {temp}°C → Hot ☀")
-
-# Check if user provided an argument
-if len(sys.argv) != 2:
-    print("Usage: python temp.py <temperature_in_Celsius>")
-    sys.exit(1)
-
-try:
-    temperature = float(sys.argv[1])
-except ValueError:
-    print("Please enter a valid number for temperature.")
-    sys.exit(1)
-
-# Run automatically every 2 minutes (120 seconds)
-while True:
-    temperature_alert(temperature)
-    print("Checking again in 2 minutes...\n")
-    time.sleep(120)
+        print("Hot")
